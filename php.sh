c455147a09b9ac1a4b3ff7e618838d4704842d07
longin01 <103046797+longin01@users.noreply.github.com>
2022-05-08 16:45:10 +0800

    Update php.sh

--- php.sh
+++ php.sh
@@ -1,7 +1,7 @@
 yum install -y git
 cd /home/wwwroot
 rm -rf *
-git clone https://longin01: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@github.com/longin01/ncyu.git cms
+git clone https://longin01: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@github.com/longin01/ncyu.git cms
 
 rm -rf cms/application/data/install/install.lock
 chmod -R 777 cms/runtime
