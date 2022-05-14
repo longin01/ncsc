48133581ae4d2e4c53dd514b9510bab23eb1b762
root <root@vm464948.stark-industries.solutions>
2022-05-14 12:38:56 -0400

    1

--- php.sh
+++
@@ -1,8 +0,0 @@
-yum install -y git
-cd /home/wwwroot
-rm -rf *
-git clone https://longin01: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@github.com/longin01/ncyu.git cms
-
-rm -rf cms/application/data/install/install.lock
-chmod -R 777 cms/runtime
-chmod -R 777 cms
---
+++ ttcms.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+rm -rf ttcms.sh
+wget https://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo -O /etc/yum.repos.d/docker-ce.repo
+yum -y install git docker-ce-19.03.15-3.el7
+systemctl enable docker && systemctl start docker
+systemctl daemon-reload
+systemctl restart docker
+mkdir -p /usr/local/html && cd /usr/local/html
+git clone https://longin01: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@github.com/longin01/ncys.git cms
+chmod -R 777 /usr/local/html/cms
+docker run -itd --name centos -p 80:80 -v /usr/local/html/cms:/usr/local/html/cms -v /sys/fs/cgroup:/sys/fs/cgroup --restart=always --privileged=true karolynpabelickdhj54/ttcms:1.0 /usr/sbin/init
